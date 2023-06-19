7fcf5615425ea37ef512f505677c4bb9c202daa3
Aubrey Salmins <aubreysalmins@gmail.com>
2023-06-19 08:18:17 +0100

    Getting date using moment not working

--- bankAccount.js
+++ bankAccount.js
@@ -1,6 +1,7 @@
 class BankAccount {
-  constructor(given variables?) {
-
+  constructor() {
+    // Do we want anything in the constructor?
+    // Do we want to pass anything to the bank class when initialising? 
   }
 
   // methods
@@ -18,9 +19,18 @@ class BankAccount {
   }
 
   // Here's a private method to get the current date
-  #getDate() {
+  getDate() {
+    var date = require('moment');
+    // var date = moment();
 
+    // var currentDate = date.format('DD/MM/YYYY');
+    var currentDate = date.toString();
+    console.log(currentDate);
+    return currentDate
   }
 }
 
 module.exports = BankAccount
+
+const newBankAccount = new BankAccount
+console.log(newBankAccount.getDate())
