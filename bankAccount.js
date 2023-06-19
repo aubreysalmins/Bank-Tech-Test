5c94dd9a88acccab02470b1087d88449978bc792
Aubrey Salmins <aubreysalmins@gmail.com>
2023-06-19 07:55:37 +0100

    Form class

---
+++ bankAccount.js
@@ -0,0 +1,26 @@
+class BankAccount {
+  constructor(given variables?) {
+
+  }
+
+  // methods
+
+  deposit() {
+
+  }
+
+  withdraw() {
+
+  }
+
+  printStatement() {
+
+  }
+
+  // Here's a private method to get the current date
+  #getDate() {
+
+  }
+}
+
+module.exports = BankAccount
